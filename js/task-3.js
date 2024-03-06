7005cb78970e919f781e88c3408c316df57e7729
Vadim <v.sokolvak@ukr.net>
2024-03-06 21:21:05 +0200

    complete task

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,21 @@
 "use strict"
 
-function getElementWidth( content, padding, border ) {
-    return ( Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2 )
+function checkForSpam(message) {
+    // приймає рядок (параметр message), 
+    // перевіряє його на вміст заборонених слів spam і sale, 
+    // і повертає результат перевірки
+
+    return (message.toLowerCase().includes("spam") || message.toLowerCase().includes("sale"))
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log('--------------------')
+console.log('------ 3 TASK ------')
+console.log('--------------------')
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
